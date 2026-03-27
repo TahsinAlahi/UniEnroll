85196b5035d6a6929cf0d29a2fedb99fff529e1c
Tahsin Alahi <alahitahsin@gmail.com>
2026-03-27 08:17:40 +0600

    feat: finished FileMemberRepo and functions

--- src/main/java/unienroll/Main.java
+++ src/main/java/unienroll/Main.java
@@ -1,24 +1,15 @@
 package unienroll;
 
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import unienroll.domain.Member;
-
-import java.io.File;
-import java.util.List;
+import unienroll.infrastructure.file.FileMemberRepository;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-
-        List<Member> members = mapper.readValue(
-                Main.class.getResourceAsStream("/data/members.json"),
-                new TypeReference<List<Member>>() {}
-        );
-
-        for(Member mem : members){
-            System.out.println(mem.toString());
+    public static void main(String[] args) {
+        try {
+            FileMemberRepository repository = FileMemberRepository.getInstance();
+            repository.deleteById("USR-103");
+            System.out.println(repository.findByEmail("david.kim@example.com"));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
--- src/main/java/unienroll/infrastructure/file/FileMemberRepository.java
+++ src/main/java/unienroll/infrastructure/file/FileMemberRepository.java
@@ -1,8 +1,103 @@
 package unienroll.infrastructure.file;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import unienroll.Main;
 import unienroll.domain.Member;
 import unienroll.repository.MemberRepository;
 
-public class FileMemberRepository {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class FileMemberRepository implements MemberRepository {
+    ObjectMapper mapper = new ObjectMapper();
+    private final List<Member> members;
+    //    Using map for faster lookup O(1)
+    private final Map<String, Member> membersByEmail = new HashMap<>();
+    private final Map<String, Member> membersById = new HashMap<>();
+
+    //    I will be using singleton pattern,
+    //    to maintain a single instance throughout the entire Project
+    private static FileMemberRepository instance;
+
+    private FileMemberRepository() throws Exception {
+        //    It's official I hate java more than my life 😭
+        members = mapper.readValue(
+                // TODO: dig more into getResourceAsStream and typeReference later
+                Main.class.getResourceAsStream("/data/members.json"),
+                new TypeReference<>() {
+                }
+        );
+
+        indexMembers();
+    }
+
+    public static FileMemberRepository getInstance() {
+        if (instance == null) {
+            try {
+                instance = new FileMemberRepository();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public String toString() {
+        //   TODO: Stream looks interesting check doc when you have time
+        return "FileMemberRepository{\n" +
+                members.stream()
+                        .map(m -> "  " + m + ", ")
+                        .reduce("", (a, b) -> a + b + "\n") +
+                "}";
+    }
+
+    @Override
+    public Member findByEmail(String email) {
+        return membersByEmail.get(email);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return membersByEmail.containsKey(email);
+    }
+
+    @Override
+    public Member save(Member entity) {
+        return null;
+    }
+
+    @Override
+    public Member findById(String id) {
+        return membersById.get(id);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return members;
+    }
+
+    @Override
+    public void deleteById(String id) {
+        //        removeIf could be easier but less declarative
+        Member removedMember = membersById.get(id);
+        members.remove(removedMember);
+        membersByEmail.remove(removedMember.getEmail());
+        membersById.remove(id);
+    }
+
+    @Override
+    public boolean existsById(String id) {
+        return membersById.containsKey(id);
+    }
+
+    private void indexMembers() {
+        for (Member m : members) {
+            membersByEmail.put(m.getEmail().toLowerCase(), m);
+            membersById.put(m.getId(), m);
+        }
+    }
 }
